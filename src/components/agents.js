e14b2899b6610fa13073ba4aea0d69d901e4208e
MattHawryszko <46821330+MattHawryszko@users.noreply.github.com>
2019-04-05 22:34:18 -0400

    Reviews And Contact

--- src/components/agents.js
+++ src/components/agents.js
@@ -6,9 +6,21 @@ import Agent2 from "../img/agent2.png";
 export default class App extends Component {
 render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-lg-6 p-1">
+      <div className="container">
+      <div class="col-lg-12">
+        <div class="pt-5">
+          <p class="h1 text-uppercase justify-content-center d-flex">CONTACT US</p>
+            
+          <div class="title-underline my-3 bg-secondary">
+            <div class="title-underline__center"></div>
+          </div>
+          <div class="mx-2">
+            <p class="text-center">Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+        <div className="row pb-5">
+          <div className="col-12 col-lg-6 p-4">
             <div class="card">
               <div class="card-body agent-bg pb-0">
                 <div className="row">
@@ -16,17 +28,17 @@ render() {
                     <img src={Agent1} className="img-fluid agent-img" />
                   </div>
                   <div className="col-7 pb-3">
-                    <h2 className="py-5 text-brown">Alice Dobber</h2>
+                    <h2 className="py-3 text-brown">Alice Dobber</h2>
                     <p className="location-text font-bold"><i className="location-text fas fa-envelope"></i> alice@luxreality.com</p>
-                    <p className="location-text font-bold pb-5"><i className="location-text fas fa-phone"></i> (282) 453 - 2234</p>
-                    <h3 className="font-bold location-text pb-5">Available 24/7</h3>
+                    <p className="location-text font-bold pb-3"><i className="location-text fas fa-phone"></i> (282) 453 - 2234</p>
+                    <h3 className="font-bold location-text pb-3">Available 24/7</h3>
                     <a className="mt-5 btn--contact-me text-uppercase" href="mailto:alice@luxreality.com">Contact Me</a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-12 col-lg-6 p-1 ">
+          <div className="col-12 col-lg-6 p-4">
             <div class="card">
               <div class="card-body agent-bg pb-0 ">
                 <div className="row">
@@ -34,10 +46,10 @@ render() {
                     <img src={Agent2} className="img-fluid agent-img" />
                   </div>
                   <div className="col-7 pb-3 ">
-                    <h2 className="py-5 text-brown">Sam Dobber</h2>
+                    <h2 className="py-3 text-brown">Sam Dobber</h2>
                     <p className="location-text font-bold"><i className="location-text fas fa-envelope"></i> sam@luxreality.com</p>
-                    <p className="location-text font-bold pb-5"><i className="location-text fas fa-phone"></i> (282) 923 - 9933</p>
-                    <h3 className="font-bold location-text pb-5">Available 24/7</h3>
+                    <p className="location-text font-bold pb-3"><i className="location-text fas fa-phone"></i> (282) 923 - 9933</p>
+                    <h3 className="font-bold location-text pb-3">Available 24/7</h3>
                     <a className="mt-5 btn--contact-me text-uppercase" href="mailto:sam@luxreality.com">Contact Me</a>
                   </div>
                 </div>
--- src/components/reviews.js
+++ src/components/reviews.js
@@ -3,9 +3,70 @@ import React, { Component } from 'react';
 export default class App extends Component {
 render() {
     return (
-        <div>
-        <h3>Reviews</h3>
-      </div>     
+      <div>
+      <div className="review-bg1"><div className="review-bg"><div className="container-fluid">
+      <div class="col-lg-12">
+        <div class="pt-2">
+          <p class="h1 text-uppercase justify-content-center d-flex">Reviews</p>
+            
+          <div class="title-underline my-3 bg-secondary">
+            <div class="title-underline__center"></div>
+          </div>
+          <div class="mx-2">
+            <p class="text-center">Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+        <div className="row pt-5">
+          <div className="col-lg-4 pb-1">
+            <div className="card">
+              <div className="row">
+                <div className="col-3 mt-5">
+                  <div className="pl-3">
+                  <h2 class="text-brown mt-2 pt-2 pl-3 far fa-user"></h2>
+                  </div>
+                </div>
+                <div className="col-9">
+                  <h2 className="pt-5 city-text">Melis Hogson</h2>
+                  <p className="pr-5 pb-5 text-light-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer condimentum sem libero. Vestibulum auctor enim ut vehicula vulputate. Fusce at.</p>              </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 pb-1">
+            <div className="card">
+              <div className="row">
+                <div className="col-3 mt-5">
+                  <div className="pl-3">
+                  <h2 class="text-brown mt-2 pt-2 pl-3 far fa-user"></h2>
+                  </div>
+                </div>
+                <div className="col-9">
+                  <h2 className="pt-5 city-text">John Hamilton</h2>
+                  <p className="pr-5 pb-5 text-light-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer condimentum sem libero. Vestibulum auctor enim ut vehicula vulputate. Fusce at.</p>              </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 pb-3">
+            <div className="card">
+              <div className="row">
+                <div className="col-3 mt-5">
+                  <div className="pl-3">
+                  <h2 class="text-brown mt-2 pt-2 pl-3 far fa-user"></h2>
+                  </div>
+                </div>
+                <div className="col-9">
+                  <h2 className="pt-5 city-text">Richard Doblin</h2>
+                  <p className="pr-5 pb-5 text-light-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer condimentum sem libero. Vestibulum auctor enim ut vehicula vulputate. Fusce at.</p>              </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>  </div>
+
+      </div>
+      
+      
+      </div>      
       );
   }
 };

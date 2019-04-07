328ea6069879c48a468ef35cf29f5f071caf3cb9
MattHawryszko <46821330+MattHawryszko@users.noreply.github.com>
2019-04-07 13:24:41 -0400

    Footer done

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,42 @@ import React, { Component } from 'react';
 export default class App extends Component {
 render() {
     return (
-        <div>
-        <h3>Footer</h3>
-      </div>     
+<footer id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 p-5">
+            <div className="row justify-content-center">
+                <h3 className="text-center text-white font-bold">Lux<span className="location-text">Reality</span></h3>   
+            </div>
+            <p class="text-light-grey text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora incidunt et perferendis ratione quod accusamus sit dolor optio empora incidunt et perferendis ratione quod accusamus sit dolor optio eveniet vero.</p>
+          </div>
+          
+          <div class="col-lg-4 text-light-grey p-lg-5 my-auto">
+            <p className="text-white">Contact us</p>
+            <p><i class="fa fa-map-marker pr-2 location-text"></i>265 Enfield Pl Mississauga, ON</p>
+            <p><i class="fa fa-phone pr-2 location-text"></i>(905) 275-2247</p>
+            <p><i class="fas fa-fax pr-2 location-text"></i>(928) 222-2233</p>
+            <p><span><i class="fa fa-envelope pr-2 location-text"></i></span><a href="mailto:info@luxreality.com" className="text-light-grey ">info@luxreality.com</a></p>
+
+          </div>
+          <div class="col-lg-4  p-lg-5 pb-5">
+            <p className="text-white">Connect with us</p>
+            <a className="p-2" href="#"><i class="fab fa-facebook location-text text-md"></i></a>
+            <a className="p-2" href="#"><i class="fab fa-twitter location-text text-md"></i></a>
+            <a className="p-2" href="#"><i class="fab fa-google-plus location-text text-md"></i></a>
+            <a className="p-2" href="#"><i class="fab fa-skype location-text text-md"></i></a>
+            <p className="pt-2 text-white">Sign up to our newsletter</p>
+            <p className="text-light-grey">Subscribe to our newsletter to recive updates on the latest properties and best realestate locations</p>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Enter email" aria-label="Enter email" aria-describedby="basic-addon2" />
+              <div class="input-group-append pl-2">
+                <button class="btn btn-outline-secondary" type="button">Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>   
+      </footer>
       );
   }
 };
--- src/scenes/index.js
+++ src/scenes/index.js
@@ -19,7 +19,6 @@ export default class App extends Component {
       	<Navbar />
         <Home />
         <Properties />
-        <Subscribe />
         <Agents />
         <Reviews />
         <Footer />
